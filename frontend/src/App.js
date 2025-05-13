0d5914712783626fff8fdccd9974078d4d0e0d3a
Peter van t Zand <petervtzand@gmail.com>
2025-05-13 20:35:43 +0200

    added frontend module

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.bettyblocks.com/hubfs/logo-red.svg" className="App-logo" alt="logo" />
+        <p>
+          Choose a model to query the results from the backend.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+<link rel="shortcut icon" href="https://www.bettyblocks.com/hubfs/betty_favicon.png"></link>
