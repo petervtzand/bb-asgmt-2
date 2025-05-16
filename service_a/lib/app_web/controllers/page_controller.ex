133af72f97ae4ea18629aaa9cf4ff8c66475a64a
Peter van t Zand <petervtzand@gmail.com>
2025-05-16 16:44:38 +0200

    Added docstring to functions

--- service_a/lib/app_web/controllers/page_controller.ex
+++ service_a/lib/app_web/controllers/page_controller.ex
@@ -3,21 +3,44 @@ defmodule AppWeb.PageController do
   require IEx
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
     render(conn, :home, layout: false)
   end
 
+  @doc """
+  Reads a file with given file path en returns the data found in file
+
+  Returns a map
+
+  ## Parameters
+  - app_file_path: The path within the application
+
+  """
   def get_data_from_file(app_file_path) do
     path = Path.join(File.cwd!(), app_file_path)
     YamlElixir.read_from_file(path)
   end
 
+  @doc """
+  Runs a redis command
+
+  Returns the result of the command
+
+  ## Parameters
+  - command_to_run: array of commands, e.g. ['PING']
+
+  """
   def run_redis_command(command_to_run) do
     Redix.start_link("redis://redis:6379/0", name: :redix)
     Redix.command(:redix, command_to_run)
   end
 
+  @doc """
+  Saves the found table info to redis
+
+  ## Parameters
+  - tables_info: mapping of the table info
+
+  """
   def save_tables_info_to_redis(tables_info) do
     # delete tables var, to make sure we don't add duplicate keys
     run_redis_command(["DEL", "tables"])
@@ -30,6 +53,12 @@ defmodule AppWeb.PageController do
     end
   end
 
+  @doc """
+  Route for reading the datastructure file and saving to redis
+
+  Returns a json object with key = 'tables' and value = list of table names
+
+  """
   @spec my_read_file_route(Plug.Conn.t(), any()) :: Plug.Conn.t()
   def my_read_file_route(conn, _params) do
     # read file and extra 'tables' data
